e52e7c21173d6e28e715a1a6c3e651ac272edd06
RedNapPanda <admin@richmondsteele.com>
2024-12-02 14:32:52 -0500

    code cleanup

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,11 +1,13 @@
+fn fold_valid<'a, I>(v: i64, dir: bool, iter: I) -> bool
+where I : Iterator<Item=&'a i64> {
+    iter.fold((v, true), |(l, res), &r| {
+        (r, res && l != r && (l < r) == dir && (l - r).abs() < 4)
+    }).1
+}
+
 fn is_valid(levels: &Vec<i64>) -> bool {
-    let dir = levels[0] < levels[1];
-    levels
-        .iter()
-        .skip(1)
-        .fold((levels[0], true), |(l, res), r| {
-            (*r, res && l != *r && (l < *r) == dir && (l - r).abs() < 4)
-        }).1
+    let iter = levels.iter().skip(1);
+    fold_valid(levels[0], levels[0] < levels[1], iter)
 }
 
 fn is_valid_skip(levels: &Vec<i64>, skip: usize) -> bool {
@@ -17,11 +19,7 @@ fn is_valid_skip(levels: &Vec<i64>, skip: usize) -> bool {
         .peekable();
     let a = *iter.next().unwrap();
     let b = **iter.peek().unwrap();
-    let dir = a < b;
-    iter.fold((a, true), |(l, res), r| {
-        (*r, res && l != *r && (l < *r) == dir && (l - r).abs() < 4)
-    })
-    .1
+    fold_valid(a, a < b, iter)
 }
 
 pub fn solve1(lines: &Vec<String>) -> Option<i64> {
