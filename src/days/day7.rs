f6a19fec9f42d00b41cfa7a71caf5cfd0572e5f9
RedNapPanda <admin@richmondsteele.com>
2024-12-07 01:20:02 -0500

    skip calculations

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 pub fn solve1(lines: &Vec<String>) -> i64 {
     solve(lines, false)
 }
@@ -16,32 +14,36 @@ fn solve(lines: &Vec<String>, concat: bool) -> i64 {
                 .split(":")
                 .map(|split| split.trim())
                 .collect::<Vec<&str>>();
-            let test = split[0].parse::<i64>().unwrap();
+            let target = split[0].parse::<i64>().unwrap();
             let nums = split[1]
                 .split_whitespace()
                 .map(|s| s.parse::<i64>().unwrap())
                 .collect::<Vec<_>>();
-            (test, nums)
+            (target, nums)
         })
-        .filter(|(test, nums)| {
-            nums[1..]
-                .iter()
-                .fold(VecDeque::from([nums[0]]), |mut acc, num| {
-                    for _ in 0..acc.len() {
-                        let prev = acc.pop_front().unwrap();
-                        if concat {
-                            let n_digits = num.checked_ilog10().unwrap_or(0) + 1;
-                            let combined = (prev * 10_i64.pow(n_digits)) + num;
-                            acc.push_back(combined);
-                        }
-                        acc.push_back(prev + num);
-                        acc.push_back(prev * num);
-                    }
-                    acc
-                })
-                .iter()
-                .any(|total| total == test)
-        })
-        .map(|(test, _)| test)
+        .filter(|(target, nums)| valid_equation(nums, *target, 1, nums[0], concat))
+        .map(|(target, _)| target)
         .sum()
+}
+
+fn valid_equation(nums: &Vec<i64>, target: i64, i: usize, val: i64, concat: bool) -> bool {
+    if i >= nums.len() {
+        return target == val
+    }
+    let mut result = false;
+    let mul = nums[i] * val;
+    if !result && mul <= target {
+        result |= valid_equation(nums, target, i+1, mul, concat)
+    }
+    let sum = nums[i] + val;
+    if !result && sum <= target {
+        result |= valid_equation(nums, target, i+1, sum, concat)
+    }
+    if !result && concat {
+        let combined = (val * 10_i64.pow(nums[i].ilog10() + 1)) + nums[i];
+        if combined <= target {
+            result |= valid_equation(nums, target, i+1, combined, concat)
+        }
+    }
+    result
 }
