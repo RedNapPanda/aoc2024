b7d5b0c4cb2d141c4f4b349e9bce16bcacc73384
RedNapPanda <admin@richmondsteele.com>
2024-12-07 01:44:00 -0500

    invert the math so we're not checking exponential branches.  Modulo to only branch when the result is possible from multiplication or concatenation

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -21,29 +21,31 @@ fn solve(lines: &Vec<String>, concat: bool) -> i64 {
                 .collect::<Vec<_>>();
             (target, nums)
         })
-        .filter(|(target, nums)| valid_equation(nums, *target, 1, nums[0], concat))
+        .filter(|(target, nums)| is_possible(nums, *target, nums.len(), concat))
         .map(|(target, _)| target)
         .sum()
 }
 
-fn valid_equation(nums: &Vec<i64>, target: i64, i: usize, val: i64, concat: bool) -> bool {
-    if i >= nums.len() {
-        return target == val
-    }
-    let mut result = false;
-    let mul = nums[i] * val;
-    if !result && mul <= target {
-        result |= valid_equation(nums, target, i+1, mul, concat)
-    }
-    let sum = nums[i] + val;
-    if !result && sum <= target {
-        result |= valid_equation(nums, target, i+1, sum, concat)
-    }
-    if !result && concat {
-        let combined = (val * 10_i64.pow(nums[i].ilog10() + 1)) + nums[i];
-        if combined <= target {
-            result |= valid_equation(nums, target, i+1, combined, concat)
+fn ten_pow_digits(val: i64) -> i64 {
+    10_i64.pow(val.ilog10() + 1)
+}
+
+fn is_possible(nums: &Vec<i64>, mut target: i64, len: usize, concat: bool) -> bool {
+    for i in (0..len).rev() {
+        if i == 0 {
+            return target == nums[0];
         }
+        let num = nums[i];
+        if (target % num == 0 && is_possible(nums, target / num, i, concat))
+            || (concat
+                && target % ten_pow_digits(num) == num
+                && is_possible(nums, target / ten_pow_digits(num), i, concat))
+        {
+            return true;
+        } else if target <= num {
+            return false;
+        }
+        target -= num;
     }
-    result
+    false
 }
