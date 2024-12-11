0b99586b611957008ee73da04f33dc3b8c325b55
RedNapPanda <admin@richmondsteele.com>
2024-12-11 10:34:29 -0500

    cleanup inserts

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -19,7 +19,8 @@ fn blink(lines: &[String], times: i64) -> i64 {
         let mut next_state = HashMap::new();
         for (&x, count) in state.iter() {
             if x == 0 {
-                next_state.insert(1, next_state.get(&1).unwrap_or(&0) + count);
+                let val = next_state.get(&1).unwrap_or(&0) + count;
+                next_state.insert(1, val);
                 continue
             }
             let digits = x.ilog10() + 1;
@@ -27,12 +28,15 @@ fn blink(lines: &[String], times: i64) -> i64 {
                 let pow = 10i64.pow(digits / 2);
                 let left = x / pow;
                 let right = x % pow;
-                next_state.insert(left, next_state.get(&left).unwrap_or(&0) + count);
-                next_state.insert(right, next_state.get(&right).unwrap_or(&0) + count);
+                let val = next_state.get(&left).unwrap_or(&0) + count;
+                next_state.insert(left, val);
+                let val = next_state.get(&right).unwrap_or(&0) + count;
+                next_state.insert(right, val);
                 continue
             }
-            let val = x * 2024;
-            next_state.insert(val, next_state.get(&val).unwrap_or(&0) + count);
+            let i = x * 2024;
+            let val = next_state.get(&i).unwrap_or(&0) + count;
+            next_state.insert(i, val);
         }
         state = next_state;
     }
