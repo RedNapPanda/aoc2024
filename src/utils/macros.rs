b1a070d609e0f93cef4d6dda9896e323c3034af7
RedNapPanda <admin@richmondsteele.com>
2024-12-14 04:09:58 -0500

    cleanup

--- src/utils/macros.rs
+++ src/utils/macros.rs
@@ -82,13 +82,11 @@ macro_rules! day_fn {
         fn day_fn(day: u8, part1: bool) -> impl Fn(&[String]) -> i64 {
             match day {
                 $(
-                $day => {
-                    if part1 {
-                        paste! { [<day $day>]::solve1 }                  
-                    } else {
-                        paste! { [<day $day>]::solve2 }
-                    }
-                },
+                $day => if part1 {
+                            paste! { [<day $day>]::solve1 }                  
+                        } else {
+                            paste! { [<day $day>]::solve2 }
+                        },
                 )*
                 _ => unimplemented!(),
             }
