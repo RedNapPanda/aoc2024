11e48e21427e2acc8b3d52f0da932ade08e42eb4
RedNapPanda <admin@richmondsteele.com>
2024-12-18 20:33:52 -0500

    day 18 p2, bruteforce lol took 12s

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -3,8 +3,10 @@ use crate::utils::grid::Grid;
 use crate::utils::node::Node;
 
 pub fn solve1(lines: &[String]) -> i64 {
-    let mut grid = Grid::with_default(71, 71, '.');
-    for line in lines[..1024].iter() {
+    let size = 71;
+    let cutoff = 1024;
+    let mut grid = Grid::with_default(size, size, '.');
+    for line in lines[..cutoff].iter() {
         if let Some(pos) = line
             .split_once(',')
             .map(|(c, r)| (r.parse::<i64>().unwrap(), c.parse::<i64>().unwrap()))
@@ -21,7 +23,7 @@ pub fn solve1(lines: &[String]) -> i64 {
         },
         |_, _| 1,
         |_, _| 0,
-        |node| node.x == 70 && node.y == 70,
+        |node| node.x == (size - 1) as i64 && node.y == (size - 1) as i64,
         false,
     ) {
         return result.cost
@@ -30,5 +32,37 @@ pub fn solve1(lines: &[String]) -> i64 {
 }
 
 pub fn solve2(lines: &[String]) -> i64 {
+    let size = 71;
+    let cutoff = 1024;
+    let mut grid = Grid::with_default(size, size, '.');
+    for line in lines[..cutoff].iter() {
+        if let Some(pos) = line
+            .split_once(',')
+            .map(|(c, r)| (r.parse::<i64>().unwrap(), c.parse::<i64>().unwrap()))
+        {
+            grid.set(&Node::from(pos), '#');
+        }
+    }
+    for i in cutoff..lines.len() {
+        let (row, col) = lines[i].split_once(',')
+            .map(|(c, r)| (r.parse::<i64>().unwrap(), c.parse::<i64>().unwrap()))
+            .unwrap();
+        grid.set(&Node::new(row, col), '#');
+        if astar(
+            &Node::new(0, 0),
+            |node| {
+                grid.neighbors_cardinal(node)
+                    .into_iter()
+                    .filter(|node| grid.get(node).is_some_and(|&c| c == '.'))
+            },
+            |_, _| 1,
+            |_, _| 0,
+            |node| node.x == (size - 1) as i64 && node.y == (size - 1) as i64,
+            false,
+        ).is_none() {
+            println!("Solution: {},{}", col, row);
+            return 0
+        }
+    }
     0
 }
