1aaf90c523556e2f1b29153f37977129edf104f8
RedNapPanda <admin@richmondsteele.com>
2024-12-22 19:46:03 -0500

    cleanup p1

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -5,15 +5,14 @@ const ITERATIONS: usize = 2000;
 const MODULO: i64 = 16777216;
 
 pub fn solve1(lines: &[String]) -> i64 {
-    let mut sum = 0;
-    for line in lines {
-        let mut secret = line.parse::<i64>().unwrap();
-        for _ in 0..ITERATIONS {
-            secret = next_secret(secret);
-        }
-        sum += secret;
-    }
-    sum
+    lines.iter()
+        .map(|line| {
+            (0..ITERATIONS)
+                .fold(line.parse::<i64>().unwrap(),
+                      |secret, _| next_secret(secret),
+                )
+        })
+        .sum::<i64>()
 }
 
 pub fn solve2(lines: &[String]) -> i64 {
@@ -21,27 +20,27 @@ pub fn solve2(lines: &[String]) -> i64 {
     let mut sequences = FxHashMap::default();
     for line in lines {
         let mut secret = line.parse::<i64>().unwrap();
-        let mut prices = vec![(secret % 10, 0); ITERATIONS+1];
+        let mut prices = vec![(secret % 10, 0); ITERATIONS + 1];
         for i in 0..ITERATIONS {
             secret = next_secret(secret);
             let price = secret % 10;
-            prices[i+1] = (price, price - prices[i].0);
+            prices[i + 1] = (price, price - prices[i].0);
         }
         let mut seen = FxHashSet::default();
         for i in 0..prices.len() - 4 {
-            let seq = (prices[i].1, prices[i+1].1, prices[i+2].1, prices[i+3].1);
-            let price = prices[i+3].0;
+            let seq = (prices[i].1, prices[i + 1].1, prices[i + 2].1, prices[i + 3].1);
+            let price = prices[i + 3].0;
             if seen.contains(&seq) {
                 continue;
             }
             seen.insert(seq);
             match sequences.entry(seq) {
-                Entry::Occupied(mut e) => {
-                    *e.get_mut() += price;
-                    sum = sum.max(*e.get_mut())
+                Entry::Occupied(mut entry) => {
+                    *entry.get_mut() += price;
+                    sum = sum.max(*entry.get_mut())
                 }
-                Entry::Vacant(e) => {
-                    e.insert(price);
+                Entry::Vacant(entry) => {
+                    entry.insert(price);
                     sum = sum.max(price)
                 }
             }
